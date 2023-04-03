bb6218679fa42730b18d104e2d8f1059cb45048c
Poipurin <b.pinorojo@gmail.com>
2023-04-03 10:36:58 -0400

    se añade estructura

--- src/vista/Aplicacion.java
+++ src/vista/Aplicacion.java
@@ -1,7 +1,22 @@
 package vista;
+import modelo.Calzado;
+import modelo.Producto;
 
 public class Aplicacion {
     public static void main(String[] args) {
+        //Instanciar un objeto de tipo producto
+        //Constructor
+        Producto producto = new Producto();
+        producto.stock = 100;
+        producto.codigo = "123ABC";
+        producto.valorBase = 10000;
+
+        Calzado calzado = new Calzado();
+
+        calzado.producto = producto;
+
+        System.out.println("Codigo producto: "+ calzado.producto.codigo);
+        System.out.println("Stock producto: "+ calzado.producto.stock);
 
     }
 }
