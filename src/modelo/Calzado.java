395b4454961d02a7c9653aac68a47082afe0d4b0
Poipurin <b.pinorojo@gmail.com>
2023-04-03 10:48:53 -0400

    se modifican los accesores

--- src/modelo/Calzado.java
+++ src/modelo/Calzado.java
@@ -1,8 +1,8 @@
 package modelo;
 
 public class Calzado {
-    public String diaVenta; //Semana o Fin de Semana
-    public int numero; //44
+    private String diaVenta; //Semana o Fin de Semana
+    private int numero; //44
 
-    public Producto producto; //{"123ABC",100,1000}
+    private Producto producto; //{"123ABC",100,1000}
 }
