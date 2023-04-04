a291fc8af0b45106825253ba9728c2b35c9aae3c
Poipurin <b.pinorojo@gmail.com>
2023-04-04 12:24:32 -0400

    se comprueba resultado de método valor a pagar

--- src/vista/Aplicacion.java
+++ src/vista/Aplicacion.java
@@ -9,13 +9,13 @@ public class Aplicacion {
 
     public static void main(String[] args) {
 
-        Deportivo deportivo = new Deportivo (new Producto("123ABC",100,10000),44,"semana","Futbol","Cuero");
+        Deportivo deportivo = new Deportivo (new Producto("123ABC",100,10000),44,"Fin de semana","Futbol","Cuero");
 
         Deportivo deportivo2 = new Deportivo();
 
         Hombre hombre = new Hombre(new Producto("222AAA",10,200000), "Fin de semana",43,"Verde");
 
-        Mujer mujer = new Mujer(new Producto("123ABC",100,10000),"semana",44,"Negro",12);
+        Mujer mujer = new Mujer(new Producto("123ABC",100,10000),"Fin de semana",44,"Negro",12);
 
         System.out.println("el valor venta del calzado deportivo durante la "+ deportivo.getDiaVenta() +
                 " es: $ "+ deportivo.valorVenta());
@@ -37,6 +37,12 @@ public class Aplicacion {
         System.out.println("Descuento calzado Hombre: $"+hombre.descuento());
 
         System.out.println("Descuento calzado Mujer: $"+mujer.descuento());
+
+        System.out.println("El valor a pagar en calzado formal de Mujer es: $"+mujer.valorAPagar());
+
+        System.out.println("El valor a pagar en calzado formal de Hombre es: $"+hombre.valorAPagar());
+
+        System.out.println("El valor a pagar en calzado deportivo es: $"+deportivo.valorAPagar());
 
     }
 }
