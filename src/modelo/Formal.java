3fcfb3239a84186dc9234d6d57d9bfd9c4ee2227
Poipurin <b.pinorojo@gmail.com>
2023-04-04 11:18:03 -0400

    se transforma a clase abstracta

--- src/modelo/Formal.java
+++ src/modelo/Formal.java
@@ -1,6 +1,6 @@
 package modelo;
 
-public class Formal extends Calzado {
+public abstract class Formal extends Calzado {
     private String color;
 
     public Formal(String diaVenta, int numero, Producto producto, String color) {
@@ -23,7 +23,15 @@ public class Formal extends Calzado {
     public int impuestoEspecifico(){
         return (int) Math.round(this.getProducto().getValorBase()*7.4/100);
     }
-    public int descuento(){
+    /*public int descuento(){
+        return 0;
+    }*/
 
-    }
+    //un método abstracto es aquel que se define en una clase pero se implementa en sus subclases
+    //no puede ser invocado desde su clase
+    //deberá ser sobreescrito por sus subclases (polimorfismo)
+    //pertenece a una clase abstracta
+    public abstract int descuento();
+    //no tiene cuerpo, se construye en las subclases
+
 }
